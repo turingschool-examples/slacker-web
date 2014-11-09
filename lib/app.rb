00ebe29892d391b740fb639f6d0609771833c6df
Steve Kinney <hello@stevekinney.net>
2014-11-09 16:46:53 -0700

    Send chat messages to JSON endpoint

--- lib/app.rb
+++ lib/app.rb
@@ -1,7 +1,13 @@
+require 'json'
+
 class Slacker < Sinatra::Base
 
   get '/' do
     erb :index
+  end
+  
+  post '/message.json' do
+    params.merge(status: :ok).to_json
   end
 
   run! if app_file == $0
