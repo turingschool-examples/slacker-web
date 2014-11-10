59ba29ab2bce5eb0b3a0a035452a1a430f80bdf6
Steve Kinney <hello@stevekinney.net>
2014-11-09 20:19:08 -0700

    Add publisher

---
+++ lib/event-listener.rb
@@ -0,0 +1,29 @@
+require 'faye'
+require 'eventmachine'
+require 'em-hiredis'
+require 'json'
+
+
+begin
+  puts "You're listening on Slacker and publishing to the web."
+  puts "Press Ctrl-C at any time to exit.\n"
+  
+  EventMachine.run do
+    redis = EM::Hiredis.connect(ENV['SLACKER_REDIS'])
+    pubsub = redis.pubsub
+  
+    client = Faye::Client.new('http://localhost:9292/faye')
+    pubsub.subscribe(:community)
+  
+    pubsub.on(:message) do |channel, message|
+      data = JSON.parse(message)
+      puts data
+      formatted_message = { author: data['user'],  body: data['msg']}
+      puts formatted_message
+      client.publish('/messages', formatted_message.to_json)
+    end
+  end
+  
+rescue Interrupt => e
+  puts "\nGoodbye..."
+end
