262b299a467adb53a5fbe8d323a5fac4386829d3
Steve Kinney <hello@stevekinney.net>
2014-11-09 17:54:28 -0700

    Remove endpoint

--- lib/app.rb
+++ lib/app.rb
@@ -1,13 +1,7 @@
-require 'json'
-
 class Slacker < Sinatra::Base
 
   get '/' do
     erb :index
-  end
-  
-  post '/message.json' do
-    params.merge(status: :ok).to_json
   end
 
   run! if app_file == $0
