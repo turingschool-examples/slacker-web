81ba3f063eeae90d27f7c1ce827c323e7bb4ebc8
Steve Kinney <hello@stevekinney.net>
2014-11-09 20:20:28 -0700

    Minor formatting fix

--- lib/event-listener.rb
+++ lib/event-listener.rb
@@ -17,7 +17,7 @@ begin
   
     pubsub.on(:message) do |channel, message|
       data = JSON.parse(message)
-      formatted_message = { author: data['user'],  body: data['msg']}
+      formatted_message = { author: data['user'],  body: data['msg'] }
       client.publish('/messages', formatted_message.to_json)
     end
   end
